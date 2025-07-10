cc27426281cf318e2cd7d2deb1180ba278481f8f
Bill Jr <oberaldo@gmail.com>
2025-07-10 13:10:55 -0300

    Implementado SweetAlert2

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,19 +1,50 @@
 @if (session('success'))
-    <div class="alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            Swal.fire({
+                title: "Pronto!",
+                html: "{!! session('success') !!}",
+                icon: "success"
+            });
+        });
+    </script>
+
+@endif
 
 @if (session('error'))
-    <div class="alert-error">
-        {{ session('error') }}
-    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            Swal.fire({
+                title: "Erro!",
+                html: "{!! session('error') !!}",
+                icon: "error"
+            });
+        });
+    </script>
 @endif
 
 @if ($errors->any())
-    <div class="alert-error">
+    {{-- <div class="alert-error">
         @foreach ($errors->all() as $error)
             {{ $error }}<br>
         @endforeach
-    </div>    
+    </div>     --}}
+
+    @php
+        $message = '';
+        foreach ($errors->all() as $error) {
+            $message .= $error . '<br>';
+        }
+    @endphp
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            Swal.fire({
+                title: "Erro!",
+                html: "{!! $message !!}",
+                icon: "error"
+            });
+        });
+    </script>
 @endif
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -29,11 +29,11 @@
                             <td class="table-actions">
                                 <a href="{{ route('user.show', ['user' => $user->id]) }}" class="btn-primary">Visualizar</a>
                                 <a href="{{ route('user.edit', ['user' => $user->id]) }}" class="btn-warning">Editar</a>
-                                <form action="{{ route('user.destroy', ['user' => $user->id]) }}" method="POST">
+                                <form id="delete-form-{{ $user->id }}" action="{{ route('user.destroy', ['user' => $user->id]) }}" method="POST">
                                     @csrf
                                     @method('PUT')
                                     {{-- <button type="button" class="btn-danger" onclick="confirmDelete({{ $user->id }})">Apagar</button> --}}
-                                    <button type="submit" class="btn-danger" onclick="return confirm('Tem certeza que deseja apagar este registro?')">Apagar</button>
+                                    <button type="button" class="btn-danger" onclick="confirmDelete({{ $user->id }})">Apagar</button>
                                 </form>
                             </td>
                         </tr>
